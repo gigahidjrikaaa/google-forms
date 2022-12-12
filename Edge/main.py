e1365f66ebc0ff5027466c18c397add84b59cc0c
Giga Hidjrika Aura Adkhy <gigahidjrikaaa@gmail.com>
2022-12-13 02:51:52 +0700

    change data

--- Edge/main.py
+++ Edge/main.py
@@ -4,12 +4,12 @@ from selenium import webdriver
 
 user = get_data()
 data = {
-  "web" : webdriver.Edge("C:\\msedgedriver.exe"),
+  "web" : webdriver.Edge("C:\\Edge Webdriver\\msedgedriver.exe"),
   "_id" : user["id"],
   "fName" : user["first"],
   "lName" : user["last"],
   "passw" : user["password"],
-  "email" : f"{user['first']}.{user['last']}@student.tdsb.on.ca", # there might be a number after the last name
+  "email" : f"{user['first']}.{user['last']}@mail.ugm.ac.id", # there might be a number after the last name
   "link" : user['link']
 }
 
